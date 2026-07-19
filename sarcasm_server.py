2bb04893ff867a67e99c423e650400ab113358c2
Asmita Banerjee <banerjeeasmita19@gmail.com>
2026-07-20 01:00:13 +0530

    Initial commit

--- sarcasm_server.py
+++ sarcasm_server.py
@@ -122,4 +122,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(port=5000, host="127.0.0.1", debug=False)
+    app.run(port=int(os.environ.get("PORT", 5000)), host="0.0.0.0", debug=False)
