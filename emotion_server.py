7db9dd44dc71c09c35fad38a9950fabae16fcc4b
Asmita Banerjee <banerjeeasmita19@gmail.com>
2026-07-20 00:59:13 +0530

    Initial commit

--- emotion_server.py
+++ emotion_server.py
@@ -164,4 +164,4 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5001, debug=False)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)), debug=False)
